5b6094853c060f06f2a482fce0d97845824186fc
CraigYeoman <yeomancraig@gmail.com>
2022-11-13 09:04:39 -0600

    Adding data to mongoDB

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
-// async errors
+require("express-async-errors");
 
 const express = require("express");
 const app = express();
+
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
@@ -16,7 +19,9 @@ app.get("/", (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">product route</a>');
 });
 
-// products route
+app.use("/api/v1/products", productsRouter);
+
+// products routee
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
@@ -25,6 +30,7 @@ const port = process.env.port || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening to port ${port}`));
   } catch (error) {
     console.log(error);
